a39e1d5816d0963dc16c7a6dad4b3bd03c21b899
Rachel <rachellizandra@outlook.com>
2022-12-17 15:34:36 -0300

    montando o tabuleiro do jogo da velha

--- src/ProjetoFinal/JogoDaVelha.java
+++ src/ProjetoFinal/JogoDaVelha.java
@@ -2,5 +2,55 @@ package ProjetoFinal;
 
 public class JogoDaVelha {
 
+    public static void main(String[] args) {
 
+        /**
+         * colocar dentro do while
+         * saber a vez de cada jogador
+         * fazer joagada
+         * ver se a jogada é valida
+         *  por posição exiteste
+         *  posição vazia
+         * ver se o jogo acabou
+         *  ver se deu velha
+         *  ver se alguem ganhou
+         *      ver na linhas
+         *      ver nas colunas
+         *      ver nas diagonais
+         * se acabou, repetir tudo(while)
+         * */
+
+        /**
+         *             _|_|_
+         *             _|_|_
+         *             _|_|_
+         */
+
+        Character[][] tab = new Character[3][3];
+
+        imprimirTabuleiro(tab);
+    }
+
+    public static void imprimirTabuleiro(Character[][] tab) {
+        System.out.println();
+
+        for(int indiceLinha = 0; indiceLinha < 3; indiceLinha++) {
+            Character[] linha = tab[indiceLinha];
+
+            for(int indiceColuna = 0; indiceColuna < 3; indiceColuna++) {
+                Character elemento = tab[indiceLinha][indiceColuna];
+
+                if(elemento == null) {
+                    System.out.print("__");
+                } else {
+                    System.out.print(elemento);
+                }
+
+                if(indiceColuna != 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
